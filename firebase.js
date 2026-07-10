cc233593353c98c62a688b9659357c2eb9aa9f43
FIKR -E-RAZA MISSION <fikrerazamission7@gmail.com>
2026-07-10 20:30:50 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,17 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-// Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyD30iXq1EmCFRe5bjJsQlsRLSbHpfIH2qI",
-  authDomain: "fikr--e-raza-mission.firebaseapp.com",
-  projectId: "fikr--e-raza-mission",
-  storageBucket: "fikr--e-raza-mission.firebasestorage.app",
-  messagingSenderId: "834592093251",
-  appId: "1:834592093251:web:81649fe63961d2d51210f6",
-  measurementId: "G-5FM0CYRN1Q"
+  apiKey: "AIzaSyDQjKf9gfkVCd33DcBkPXaqxYqC8w9tbg8",
+  authDomain: "fikr--e-raza-mission-91f2c.firebaseapp.com",
+  projectId: "fikr--e-raza-mission-91f2c",
+  storageBucket: "fikr--e-raza-mission-91f2c.firebasestorage.app",
+  messagingSenderId: "904569522186",
+  appId: "1:904569522186:web:7e8a1f1fb105f6e13137f3"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export
 export const auth = getAuth(app);
 export const db = getFirestore(app);
